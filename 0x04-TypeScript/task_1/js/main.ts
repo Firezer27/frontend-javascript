6976cfb338873ae49c7f9562a08e3b71b8a83b13
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-08 11:49:39 +0300

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,20 @@
-interface Teacher{
-    firstName: string;
-    lastname: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any;
+// Define Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string; //
+  [key: string]: any; //
 }
 
+
 const teacher3: Teacher = {
-    firstName: "John",
-    lastname: "Doe",
-    fullTimeEmployee: false,
-    location: "London",
-    contract: false
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: false,
+  location: "London",
+  contract: false, //
 };
 
 console.log(teacher3);
