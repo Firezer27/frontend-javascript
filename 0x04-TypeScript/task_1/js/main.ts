89ba1b13a7e96055d40675c67a799eb9510b461a
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-08 12:02:19 +0300

    create interface and function in main.ts file

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,4 +19,29 @@ const teacher3: Teacher = {
 console.log(teacher3);
 
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
 
+// Create object from Directors
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+// --------------- Task 3: printTeacher Function -----------------
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+
+console.log(printTeacher("John", "Doe"));
