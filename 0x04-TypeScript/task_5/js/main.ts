44944a3c4a8d998248929ccd94fcf9439c0cdd7f
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-09 17:23:37 +0300

    create task-5

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,42 @@
+interface MajorCredits {
+  credits: number;
+  brand: "major";
+}
+
+interface MinorCredits {
+  credits: number;
+  brand: "minor";
+}
+
+function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "major",
+  };
+}
+
+function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "minor",
+  };
+}
+
+
+const math: MajorCredits = { credits: 3, brand: "major" };
+const physics: MajorCredits = { credits: 4, brand: "major" };
+
+const totalMajor = sumMajorCredits(math, physics);
+console.log(totalMajor);
+
+const art: MinorCredits = { credits: 2, brand: "minor" };
+const music: MinorCredits = { credits: 3, brand: "minor" };
+
+const totalMinor = sumMinorCredits(art, music);
+console.log(totalMinor);
