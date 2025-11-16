a3b54421f87b704ac8459de195ca74c3b2b54c26
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-16 15:37:40 +0300

    update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,3 @@
-
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -8,32 +7,26 @@ interface Teacher {
   [key: string]: any;
 }
 
-
 interface Directors extends Teacher {
   numberOfReports: number;
 }
-
 
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName} ${lastName}`;
 };
-
 
 interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-
 interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
 }
-
 
 class StudentClass implements StudentClassInterface {
   constructor(private firstName: string, private lastName: string) {}
@@ -46,3 +39,14 @@ class StudentClass implements StudentClassInterface {
     return this.firstName;
   }
 }
+
+const teacher3: Teacher = {
+  firstName: "John",
+  fullTimeEmployee: false,
+  lastName: "Doe",
+  location: "London",
+  contract: false,
+};
+
+console.log(teacher3);
+console.log(printTeacher("John", "Doe"));
